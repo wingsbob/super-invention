21c23d92ad14dc8d43453d9ff3fd31686034e7c1
Matthew Bull <matt_wings@hotmail.com>
2017-06-07 23:00:31 +0100

    Added an Update Events endpoint

--- mock-api/server.ts
+++ mock-api/server.ts
@@ -1,37 +1,39 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import clients from './clients';
 import apps from './apps';
 import events from './events';
 
+const createGetById = (idName: string, collection: {id: string}[]) =>
+  (req, res) => {
+    const requestedId = req.params[idName];
+    const target = collection.find(({id}) => id === requestedId);
+
+    if (target) res.json(target);
+    else res.status(404).end();
+  };
+
 export default () => {
   const app = express();
 
-  app.get('/clients', (req, res) => {
-    res.json(clients);
-  });
-  app.get('/client/:clientId', (req, res) => {
-    const {clientId} = req.params
-    const client = clients.find(({id}) => id === clientId);
+  app.use(bodyParser.json());
+  app.get('/apps', (req, res) => res.json(apps));
+  app.get('/clients', (req, res) => res.json(clients));
 
-    if (client) res.json(client);
-    else res.status(404).end();
-  });
-  app.get('/apps', (req, res) => {
-    res.json(apps);
-  });
-  app.get('/app/:appId', (req, res) => {
-    const {appId} = req.params
-    const app = apps.find(({id}) => id === appId);
+  app.get('/client/:clientId', createGetById('clientId', clients))
+  app.get('/app/:appId', createGetById('appId', apps));
+  app.get('/events/:appId', createGetById('appId', events));
 
-    if (app) res.json(app);
-    else res.status(404).end();
-  });
-  app.get('/events/:appId', (req, res) => {
+  app.put('/events/:appId', (req, res) => {
     const {appId} = req.params
     const appEvents = events.find(({id}) => id === appId);
 
-    if (appEvents) res.json(appEvents);
-    else res.status(404).end();
+    if (!appEvents) res.status(404).send(`event with id: ${appId} does not exist`);
+    else {
+      const update = req.body;
+      if (appId !== update.id) res.status(400).send(`cannot modify appId for an event`);
+      else res.status(204).send();
+    }
   });
 
   return app.listen(3000);
--- mock-api/test/server.Spec.ts
+++ mock-api/test/server.Spec.ts
@@ -197,7 +197,7 @@ describe('the mock api server', () => {
         });
     });
   });
-  describe('GET /events/:appsId', () => {
+  describe('GET /events/:appId', () => {
     it('returns a 404 when no client is specified', (done) => {
       const app = server();
 
@@ -239,6 +239,66 @@ describe('the mock api server', () => {
         .get('/events/316dc366-4a24-463f-b87c-2e8f2c11a903')
         .expect(200)
         .expect(events.find(({id}) => id === '316dc366-4a24-463f-b87c-2e8f2c11a903'))
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+  });
+  describe('PUT /events/:appId', () => {
+    it('returns a 404 when no client is specified', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/events/')
+        .expect(404)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns a 404 when the client specified does not exist', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/events/2e9466b1-6fae-4639-80c8-101181688d06')
+        .expect(404)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns a 400 when the update tries to change the Id', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/events/316dc366-4a24-463f-b87c-2e8f2c11a903')
+        .send({id: 'not-the-same'})
+        .expect(400)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns a 400 when the update is not an object', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/events/316dc366-4a24-463f-b87c-2e8f2c11a903')
+        .send(null)
+        .expect(400)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns a 204 when the update has the same Id', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/events/316dc366-4a24-463f-b87c-2e8f2c11a903')
+        .send({id: '316dc366-4a24-463f-b87c-2e8f2c11a903'})
+        .expect(204)
         .end((err) => {
           app.close();
           done(err);
