ed43b49007b7c2d3ae76a623bd117e3dbda52452
Matthew Bull <matt_wings@hotmail.com>
2017-06-08 09:20:11 +0100

    Added an updateApp method

--- mock-api/server.ts
+++ mock-api/server.ts
@@ -13,6 +13,26 @@ const createGetById = (idName: string, collection: {id: string}[]) =>
     else res.status(404).end();
   };
 
+const createUpdateById = (idName: string, collection: {id: string}[]) =>
+  (req: express.Request, res: express.Response) => {
+    const idToUpdate = req.params[idName]
+    const eventsIndex = collection.findIndex(({id}) => id === idToUpdate);
+
+    if (eventsIndex === -1) res.status(404).send(`event with id: ${idToUpdate} does not exist`);
+    else {
+      const update = req.body;
+      if (idToUpdate !== update.id) res.status(400).send(`cannot modify the id`);
+      else {
+        const events = collection.splice(eventsIndex, 1)[0];
+        collection.push({
+          ...events,
+          ...update
+        });
+        res.status(204).send();
+      }
+    }
+  }
+
 export default () => {
   const instanceClients: typeof clients = JSON.parse(JSON.stringify(clients))
   const instanceApps: typeof apps = JSON.parse(JSON.stringify(apps))
@@ -27,24 +47,8 @@ export default () => {
   app.get('/app/:appId', createGetById('appId', instanceApps));
   app.get('/events/:appId', createGetById('appId', instanceEvents));
 
-  app.put('/events/:appId', (req, res) => {
-    const {appId} = req.params
-    const eventsIndex = instanceEvents.findIndex(({id}) => id === appId);
-
-    if (eventsIndex === -1) res.status(404).send(`event with id: ${appId} does not exist`);
-    else {
-      const update = req.body;
-      if (appId !== update.id) res.status(400).send(`cannot modify appId for an event`);
-      else {
-        const events = instanceEvents.splice(eventsIndex, 1)[0];
-        instanceEvents.push({
-          ...events,
-          ...update
-        });
-        res.status(204).send();
-      }
-    }
-  });
+  app.put('/events/:appId', createUpdateById('appId', instanceEvents));
+  app.put('/app/:appId', createUpdateById('appId', instanceApps));
 
   return app.listen(3000);
 };
--- mock-api/test/server.Spec.ts
+++ mock-api/test/server.Spec.ts
@@ -322,4 +322,74 @@ describe('the mock api server', () => {
         );
     });
   });
+  describe('PUT /app/:appId', () => {
+    it('returns a 404 when no client is specified', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/app/')
+        .expect(404)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns a 404 when the client specified does not exist', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/app/2e9466b1-6fae-4639-80c8-101181688d06')
+        .expect(404)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns a 400 when the update tries to change the Id', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/app/316dc366-4a24-463f-b87c-2e8f2c11a903')
+        .send({id: 'not-the-same'})
+        .expect(400)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns a 204 when the update has the same Id', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/app/316dc366-4a24-463f-b87c-2e8f2c11a903')
+        .send({id: '316dc366-4a24-463f-b87c-2e8f2c11a903'})
+        .expect(204)
+        .end((err) => {
+          app.close();
+          done(err);
+        });
+    });
+    it('returns returns the updated apps ', (done) => {
+      const app = server();
+
+      request(app)
+        .put('/app/316dc366-4a24-463f-b87c-2e8f2c11a903')
+        .send({
+          id: '316dc366-4a24-463f-b87c-2e8f2c11a903',
+          supportedDevices: 3
+        })
+        .end((err) =>
+          request(app)
+            .get('/app/316dc366-4a24-463f-b87c-2e8f2c11a903')
+            .expect({
+              ...apps.find(({id}) => id === '316dc366-4a24-463f-b87c-2e8f2c11a903'),
+              supportedDevices: 3
+            })
+            .end(err => {
+              app.close();
+              done(err);
+            })
+        );
+    });
+  });
 });
